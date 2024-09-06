c15b789cdac5c664540150051620bd1abc91cf35
Takeru Ohta <phjgt308@gmail.com>
2024-09-06 23:12:28 +0900

    Update mio features

--- src/node.rs
+++ src/node.rs
@@ -1,7 +1,10 @@
+use std::net::{SocketAddr, TcpListener};
+
+use mio::Poll;
 use raftbare::{NodeId, Role};
 use serde::{Deserialize, Serialize};
 
-pub trait Machine: Serialize + for<'a> Deserialize<'a> {
+pub trait Machine: Default + Serialize + for<'a> Deserialize<'a> {
     type Command: Serialize + for<'a> Deserialize<'a>;
 
     fn apply(&mut self, command: Self::Command);
@@ -11,16 +14,25 @@ pub trait Machine: Serialize + for<'a> Deserialize<'a> {
 pub struct Node<M> {
     inner: raftbare::Node,
     machine: M,
+    listener: TcpListener,
+    poll: Poll,
 }
 
 impl<M: Machine> Node<M> {
     pub const UNINIT_NODE_ID: NodeId = NodeId::new(u64::MAX);
 
-    pub fn new(machine: M) -> Self {
-        Self {
+    pub fn new(addr: SocketAddr) -> serde_json::Result<Self> {
+        let listener = TcpListener::bind(addr).map_err(serde_json::Error::io)?;
+        listener
+            .set_nonblocking(true)
+            .map_err(serde_json::Error::io)?;
+
+        Ok(Self {
             inner: raftbare::Node::start(Self::UNINIT_NODE_ID),
-            machine,
-        }
+            machine: M::default(),
+            listener,
+            poll: Poll::new().map_err(serde_json::Error::io)?,
+        })
     }
 
     pub fn id(&self) -> NodeId {
@@ -29,6 +41,18 @@ impl<M: Machine> Node<M> {
 
     pub fn role(&self) -> Role {
         self.inner.role()
+    }
+
+    pub fn listener(&self) -> &TcpListener {
+        &self.listener
+    }
+
+    pub fn poll(&self) -> &Poll {
+        &self.poll
+    }
+
+    pub fn poll_mut(&mut self) -> &mut Poll {
+        &mut self.poll
     }
 
     // TODO: current_term(), etc
