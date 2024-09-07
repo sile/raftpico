994367281b11233c6bc2c917db362798529ec06d
Takeru Ohta <phjgt308@gmail.com>
2024-09-07 15:27:12 +0900

    [WIP] Add mio handling

--- src/node.rs
+++ src/node.rs
@@ -1,6 +1,6 @@
-use std::net::{SocketAddr, TcpListener};
+use std::{net::SocketAddr, time::Duration};
 
-use mio::Poll;
+use mio::{net::TcpListener, Events, Interest, Poll, Token};
 use raftbare::{NodeId, Role};
 use serde::{Deserialize, Serialize};
 
@@ -15,7 +15,7 @@ impl MachineContext {
     }
 }
 
-pub trait Machine: Default + Serialize + for<'a> Deserialize<'a> {
+pub trait Machine: Serialize + for<'a> Deserialize<'a> {
     type Command: Serialize + for<'a> Deserialize<'a>;
 
     fn apply(&mut self, ctx: &MachineContext, command: Self::Command);
@@ -26,23 +26,33 @@ pub struct Node<M> {
     inner: raftbare::Node,
     machine: M,
     listener: TcpListener,
-    poll: Poll,
+    poller: Poll,
+    events: Option<Events>,
+    next_token: Token,
 }
 
 impl<M: Machine> Node<M> {
     pub const UNINIT_NODE_ID: NodeId = NodeId::new(u64::MAX);
 
-    pub fn new(addr: SocketAddr) -> serde_json::Result<Self> {
-        let listener = TcpListener::bind(addr).map_err(serde_json::Error::io)?;
-        listener
-            .set_nonblocking(true)
-            .map_err(serde_json::Error::io)?;
+    const SERVER_TOKEN: Token = Token(0);
 
+    // TODO: io result ?
+    pub fn new(addr: SocketAddr, machine: M) -> serde_json::Result<Self> {
+        let mut listener = TcpListener::bind(addr).map_err(serde_json::Error::io)?;
+
+        let poller = Poll::new().map_err(serde_json::Error::io)?;
+        let events = Events::with_capacity(256); // TODO: configurable
+        poller
+            .registry()
+            .register(&mut listener, Self::SERVER_TOKEN, Interest::READABLE)
+            .map_err(serde_json::Error::io)?;
         Ok(Self {
             inner: raftbare::Node::start(Self::UNINIT_NODE_ID),
-            machine: M::default(),
+            machine,
             listener,
-            poll: Poll::new().map_err(serde_json::Error::io)?,
+            poller,
+            events: Some(events),
+            next_token: Token(1),
         })
     }
 
@@ -58,17 +68,98 @@ impl<M: Machine> Node<M> {
         &self.listener
     }
 
-    pub fn poll(&self) -> &Poll {
-        &self.poll
+    pub fn poller(&self) -> &Poll {
+        &self.poller
     }
 
-    pub fn poll_mut(&mut self) -> &mut Poll {
-        &mut self.poll
+    pub fn poller_mut(&mut self) -> &mut Poll {
+        &mut self.poller
     }
 
     // TODO: current_term(), etc
 
     pub fn machine(&self) -> &M {
         &self.machine
+    }
+
+    pub fn create_cluster(&mut self) -> bool {
+        if self.id() != Self::UNINIT_NODE_ID {
+            return false;
+        }
+
+        let node_id = NodeId::new(0);
+        self.inner = raftbare::Node::start(node_id);
+        let mut promise = self.inner.create_cluster(&[node_id]);
+        promise.poll(&mut self.inner);
+        assert!(!promise.is_pending());
+
+        promise.is_accepted()
+    }
+
+    pub fn poll_one(&mut self, timeout: Option<Duration>) -> std::io::Result<bool> {
+        let Some(mut events) = self.events.take() else {
+            todo!();
+        };
+        self.poller.poll(&mut events, timeout)?;
+        let mut did_something = false;
+
+        for event in events.iter() {
+            did_something = true;
+            match event.token() {
+                Self::SERVER_TOKEN => self.handle_listener()?,
+                _ => todo!(),
+            }
+        }
+
+        self.events = Some(events);
+        Ok(did_something)
+    }
+
+    fn handle_listener(&mut self) -> std::io::Result<()> {
+        loop {
+            let Some((stream, _addr)) = would_block(self.listener.accept())? else {
+                return Ok(());
+            };
+            todo!()
+        }
+    }
+}
+
+fn would_block<T>(result: std::io::Result<T>) -> std::io::Result<Option<T>> {
+    match result {
+        Ok(v) => Ok(Some(v)),
+        Err(e) if e.kind() == std::io::ErrorKind::WouldBlock => Ok(None),
+        Err(e) => Err(e),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use orfail::OrFail;
+
+    impl Machine for () {
+        type Command = ();
+
+        fn apply(&mut self, _ctx: &MachineContext, _command: Self::Command) {}
+    }
+
+    #[test]
+    fn create_cluster() -> orfail::Result<()> {
+        let mut node = Node::new(auto_addr(), ()).or_fail()?;
+        assert_eq!(node.id(), Node::<()>::UNINIT_NODE_ID);
+
+        node.create_cluster().or_fail()?;
+        assert_eq!(node.id(), NodeId::new(0));
+
+        Ok(())
+    }
+
+    fn auto_addr() -> SocketAddr {
+        addr("127.0.0.1:0")
+    }
+
+    fn addr(s: &str) -> SocketAddr {
+        s.parse().expect("parse addr")
     }
 }
