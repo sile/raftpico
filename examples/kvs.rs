526eee9b85e73f3aa6ea28d80170464490fe572a
Takeru Ohta <phjgt308@gmail.com>
2024-09-16 17:47:39 +0900

    Update example

--- examples/kvs.rs
+++ examples/kvs.rs
@@ -1,67 +1,60 @@
-// use std::{collections::HashMap, net::SocketAddr};
+use std::{collections::HashMap, net::SocketAddr};
 
-// use clap::Parser;
-// use orfail::OrFail;
-// use raftpico::node::{Machine, MachineContext};
-// use serde::{Deserialize, Serialize};
+use clap::Parser;
+use orfail::OrFail;
+use raftpico::{Context, Machine, RaftServer};
+use serde::{Deserialize, Serialize};
 
-// #[derive(Parser)]
-// struct Args {
-//     listen_addr: SocketAddr,
-// }
-
-fn main() -> orfail::Result<()> {
-    // let args = Args::parse();
-    // let mut node = raftpico::node::Node::new(args.listen_addr, KvsMachine::default()).or_fail()?;
-    // loop {
-    //     node.poll_one(None).or_fail()?;
-    // }
-    Ok(())
+#[derive(Parser)]
+struct Args {
+    listen_addr: SocketAddr,
 }
 
-// #[derive(Debug, Default, Serialize, Deserialize)]
-// struct KvsMachine {
-//     entries: HashMap<String, serde_json::Value>,
-// }
+fn main() -> orfail::Result<()> {
+    let args = Args::parse();
+    let mut node = RaftServer::start(args.listen_addr, KvsMachine::default()).or_fail()?;
+    loop {
+        node.poll(None).or_fail()?;
+    }
+}
 
-// impl Machine for KvsMachine {
-//     type Command = KvsCommand;
+#[derive(Debug, Default, Serialize, Deserialize)]
+struct KvsMachine {
+    entries: HashMap<String, serde_json::Value>,
+}
 
-//     fn apply(&mut self, ctx: &mut MachineContext, command: Self::Command) {
-//         match command {
-//             KvsCommand::Put { key, value } => {
-//                 let value = self.entries.insert(key, value);
-//                 ctx.reply(&serde_json::json!({
-//                    "old_value": value,
-//                 }));
-//             }
-//             KvsCommand::Get { key } => {
-//                 let value = self.entries.get(&key);
-//                 ctx.reply(&serde_json::json!({
-//                    "value": value,
-//                 }));
-//             }
-//             KvsCommand::Delete { key } => {
-//                 let value = self.entries.remove(&key);
-//                 ctx.reply(&serde_json::json!({
-//                    "old_value": value,
-//                 }));
-//             }
-//         }
-//     }
-// }
+impl Machine for KvsMachine {
+    type Input = KvsInput;
 
-// #[derive(Debug, Serialize, Deserialize)]
-// #[serde(rename_all = "snake_case")]
-// enum KvsCommand {
-//     Put {
-//         key: String,
-//         value: serde_json::Value,
-//     },
-//     Get {
-//         key: String,
-//     },
-//     Delete {
-//         key: String,
-//     },
-// }
+    fn handle_input(&mut self, ctx: &mut Context, input: Self::Input) {
+        match input {
+            KvsInput::Put { key, value } => {
+                let value = self.entries.insert(key, value);
+                ctx.output(&value);
+            }
+            KvsInput::Get { key } => {
+                let value = self.entries.get(&key);
+                ctx.output(&value);
+            }
+            KvsInput::Delete { key } => {
+                let value = self.entries.remove(&key);
+                ctx.output(&value);
+            }
+        }
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
+enum KvsInput {
+    Put {
+        key: String,
+        value: serde_json::Value,
+    },
+    Get {
+        key: String,
+    },
+    Delete {
+        key: String,
+    },
+}
