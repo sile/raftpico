ff84124f5da34210431f02b46feb938e5ec8c78b
Takeru Ohta <phjgt308@gmail.com>
2024-11-19 09:42:09 +0900

    Add command() test

--- src/lib.rs
+++ src/lib.rs
@@ -191,70 +191,70 @@ mod tests {
         assert!(server1.node().is_some());
     }
 
-    // #[test]
-    // fn command() {
-    //     let mut servers = Vec::new();
-    //     let mut server0 = Server::start(auto_addr(), 0).expect("start() failed");
+    #[test]
+    fn command() {
+        let mut servers = Vec::new();
+        let mut server0 = RaftServer::start(auto_addr(), 0).expect("start() failed");
 
-    //     // Create a cluster.
-    //     let server_addr0 = server0.addr();
-    //     let handle = std::thread::spawn(move || {
-    //         rpc::<CreateClusterResult>(server_addr0, Request::create_cluster(request_id(0), None))
-    //     });
-    //     while !handle.is_finished() {
-    //         server0.poll(POLL_TIMEOUT).expect("poll() failed");
-    //     }
-    //     servers.push(server0);
+        // Create a cluster.
+        let server_addr0 = server0.listen_addr();
+        let handle = std::thread::spawn(move || {
+            rpc::<CreateClusterResult>(server_addr0, Request::create_cluster(request_id(0), None))
+        });
+        while !handle.is_finished() {
+            server0.poll(POLL_TIMEOUT).expect("poll() failed");
+        }
+        servers.push(server0);
 
-    //     // Add two servers to the cluster.
-    //     let server1 = Server::start(auto_addr(), 0).expect("start() failed");
-    //     let server2 = Server::start(auto_addr(), 0).expect("start() failed");
-    //     let server_addr1 = server1.addr();
-    //     let server_addr2 = server2.addr();
-    //     let handle = std::thread::spawn(move || {
-    //         let mut contact_addr = server_addr0;
-    //         for addr in [server_addr1, server_addr2] {
-    //             let result: AddServerResult =
-    //                 rpc(contact_addr, Request::add_server(request_id(0), addr));
-    //             assert_eq!(result.error, None);
-    //             contact_addr = addr;
+        // Add two servers to the cluster.
+        let server1 = RaftServer::start(auto_addr(), 0).expect("start() failed");
+        let server2 = RaftServer::start(auto_addr(), 0).expect("start() failed");
+        let server_addr1 = server1.listen_addr();
+        let server_addr2 = server2.listen_addr();
+        let handle = std::thread::spawn(move || {
+            let mut contact_addr = server_addr0;
+            for addr in [server_addr1, server_addr2] {
+                let result: AddServerResult =
+                    rpc(contact_addr, Request::add_server(request_id(0), addr));
+                assert_eq!(result.error, None);
+                contact_addr = addr;
 
-    //             // TODO:
-    //             std::thread::sleep(Duration::from_millis(100));
-    //         }
-    //     });
-    //     servers.push(server1);
-    //     servers.push(server2);
+                // TODO:
+                std::thread::sleep(Duration::from_millis(100));
+            }
+        });
+        servers.push(server1);
+        servers.push(server2);
 
-    //     while !handle.is_finished() {
-    //         for server in &mut servers {
-    //             server.poll(POLL_TIMEOUT).expect("poll() failed");
-    //         }
-    //     }
-    //     for server in &servers {
-    //         assert!(server.node().is_some());
-    //     }
+        while !handle.is_finished() {
+            for server in &mut servers {
+                server.poll(POLL_TIMEOUT).expect("poll() failed");
+            }
+        }
+        for server in &servers {
+            assert!(server.node().is_some());
+        }
 
-    //     // Propose commands.
-    //     let addrs = servers.iter().map(|s| s.addr()).collect::<Vec<_>>();
-    //     let handle = std::thread::spawn(move || {
-    //         for (i, addr) in addrs.into_iter().enumerate() {
-    //             let _v: serde_json::Value = rpc(
-    //                 addr,
-    //                 Request::command(request_id(0), &i).expect("unreachable"),
-    //             );
-    //         }
-    //     });
+        // Propose commands.
+        let addrs = servers.iter().map(|s| s.listen_addr()).collect::<Vec<_>>();
+        let handle = std::thread::spawn(move || {
+            for (i, addr) in addrs.into_iter().enumerate() {
+                let _v: serde_json::Value = rpc(
+                    addr,
+                    Request::command(request_id(0), &i).expect("unreachable"),
+                );
+            }
+        });
 
-    //     while !handle.is_finished() {
-    //         for server in &mut servers {
-    //             server.poll(POLL_TIMEOUT).expect("poll() failed");
-    //         }
-    //     }
-    //     for server in &servers {
-    //         assert_eq!(*server.machine(), 0 + 1 + 2);
-    //     }
-    // }
+        while !handle.is_finished() {
+            for server in &mut servers {
+                server.poll(POLL_TIMEOUT).expect("poll() failed");
+            }
+        }
+        for server in &servers {
+            assert_eq!(*server.machine(), 0 + 1 + 2);
+        }
+    }
 
     #[test]
     fn re_election() {
