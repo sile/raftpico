81b28b1c8ad3fd1623309cbc2b04535af29e3575
Takeru Ohta <phjgt308@gmail.com>
2024-11-23 17:43:08 +0900

    Remove stats module

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,9 @@ pub mod command;
 mod machine;
 pub mod message;
 pub mod server;
-pub mod stats;
 pub mod storage; // TODO
 
 pub use machine::{Context2, InputKind, Machine2};
 pub use message::Request;
 pub use server::Server;
-pub use stats::ServerStats;
 pub use storage::FileStorage;
