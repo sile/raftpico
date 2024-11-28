dca0846afb765698069d7a8adc303f0695ac932c
Takeru Ohta <phjgt308@gmail.com>
2024-11-28 19:17:12 +0900

    Remove `Record`'s type parameter

--- src/storage.rs
+++ src/storage.rs
@@ -33,8 +33,7 @@ impl FileStorage {
     pub fn install_snapshot(&mut self, snapshot: InstallSnapshotParams) -> std::io::Result<()> {
         // TODO: temorary file and move (and writing the temporary file on a worker thread)
         self.file.inner().set_len(0)?;
-        self.file
-            .write_value(&Record::<LogEntries>::Snapshot(snapshot))?;
+        self.file.write_value(&Record::Snapshot(snapshot))?;
         self.maybe_fsync()?;
         Ok(())
     }
@@ -51,30 +50,27 @@ impl FileStorage {
         raft_log_entries: &raftbare::LogEntries,
         commands: &crate::server::Commands,
     ) -> std::io::Result<()> {
-        let entries =
-            Record::<_>::LogEntries(LogEntries::from_raftbare(raft_log_entries, commands)?);
+        let entries = Record::LogEntries(LogEntries::from_raftbare(raft_log_entries, commands)?);
         self.file.write_value(&entries)?;
         self.maybe_fsync()?;
         Ok(())
     }
 
     pub fn save_node_id(&mut self, node_id: NodeId) -> std::io::Result<()> {
-        self.file
-            .write_value(&Record::<LogEntries>::NodeId(node_id.into()))?;
+        self.file.write_value(&Record::NodeId(node_id.into()))?;
         self.maybe_fsync()?;
         Ok(())
     }
 
     pub fn save_current_term(&mut self, term: Term) -> std::io::Result<()> {
-        self.file
-            .write_value(&Record::<LogEntries>::Term(term.into()))?;
+        self.file.write_value(&Record::Term(term.into()))?;
         self.maybe_fsync()?;
         Ok(())
     }
 
     pub fn save_voted_for(&mut self, voted_for: Option<NodeId>) -> std::io::Result<()> {
         self.file
-            .write_value(&Record::<LogEntries>::VotedFor(voted_for.map(|n| n.into())))?;
+            .write_value(&Record::VotedFor(voted_for.map(|n| n.into())))?;
         self.maybe_fsync()?;
         Ok(())
     }
@@ -101,11 +97,11 @@ impl FileStorage {
 
 // TODO: remove parameter
 #[derive(Debug, Serialize, Deserialize)]
-enum Record<T> {
+enum Record {
     NodeId(u64),
     Term(u64),
     VotedFor(Option<u64>),
-    LogEntries(T),
+    LogEntries(LogEntries),
     Snapshot(InstallSnapshotParams),
 }
 
